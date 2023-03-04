b491aaa15bf0329b3123fa6ca755c2cadaf1f683
vicentemarti31416 <vicentemarti31416@gmail.com>
2023-03-04 11:30:33 +0100

    adding code

--- ejerciciosJs/S1/extra/exercise-1.js
+++ ejerciciosJs/S1/extra/exercise-1.js
@@ -1,5 +1,5 @@
 const users = [{name: "Abel", years: 43}, {name: "Maria", years: 18}, {name: "Pedro", years: 14}, {name: "Samantha", years: 32}, {name: "Raquel", years: 16}];
 
-for (user of users) {
+for (let user of users) {
     user.years >= 18? console.log("Usuario mayor de edad:" + user.name) : console.log("Usuario menor de edad:" + user.name);
 }
